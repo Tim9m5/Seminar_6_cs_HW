921ac0c0b6c83cb10f5b7da8a44b7e667bbd3f5a
Timur Halitov <halitovtimur@list.ru>
2023-03-05 22:49:12 +0500

    HomeWork

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 41. Пользователь вводит М чисел с клавиатуры.
 // Нужно посчитать сколько из этих чисел больше 0.
-
+/*
 Console.WriteLine("Введите солько чисел вы хотите проверить: ");
 int size = Convert.ToInt32 (Console.ReadLine());
 Console.WriteLine("Введите проверяемые числа, каждое на новой строке: ");
@@ -25,3 +25,39 @@ int count = 0;
     }
 }
 Console.WriteLine($"Введено {count} положительных чисел ");
+*/
+//----------------------------------------------------------------
+
+// Задача 43. Напишите программу, которая найдет точку пересечения
+// двух прямых, заданных уравнениями y=k1*x+b1, y=k2*x+b2.
+// Значения k1, k2, b1, b2 задаются пользователем.
+/*
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+if (k1 == k2 && b1 != b2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else if (k1 == k2 && b1 == b2)
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else
+{
+    Console.WriteLine($"Координаты пересечения прямых: x = {x}; y = {y}");
+}
+
+*/
+
